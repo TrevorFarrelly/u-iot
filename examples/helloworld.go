45b0b75a706d6bb76354248a55a0da42b667d0f7
Trevor Farrelly <farrellytrevor@gmail.com>
2020-03-24 16:29:15 -0400

    Write helloworld example demonstrating interface
    Signed-off-by: Trevor Farrelly <farrellytrevor@gmail.com>

---
+++ examples/helloworld.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+  "log"
+  "os"
+  "strconv"
+
+  uiot "github.com/TrevorFarrelly/u-iot/lib/uiot-go"
+)
+
+// functions that this device performs. Due to Go's strict typing, parameters
+// must be variadic. As long as the signature is defined properly, the library
+// will verify that you get the number of variables you want, and they are within
+// the range you want.
+func Hello(args ...int) {
+	log.Println("Hello, World! You sent %d", args[0])
+}
+
+func main() {
+  name := os.Args[1]
+  port, _ := strconv.Atoi(os.Args[2])
+
+  // create a new device.
+  // We specify the port the RPC server will listen on, as well as a device type
+  // and location for convenience
+  d := uiot.NewDevice(name, port, uiot.Light, uiot.Living)
+
+  // add a function to the new device
+  // We provide a name for the function and the expected parameters. Each
+  // parameter has a range of values it can take. u-iot handles input sanitization
+  // internally.
+  d.AddFunction("hello", Hello, uiot.Param{0,256})
+
+  // connect to the network.
+  // we get a Network instance back. This instance is updated in the background
+  // automaticaly as new devices connect.
+  net, err := uiot.Bootstrap(d)
+  if err != nil {
+    log.Printf("could not bootstrap: %v", err)
+  }
+  // print network info when a new device joins.
+  // the Network instance contains a channel that triggers when a new device is
+  // added to its internal list. This is an easy, if basic, way to create an
+  // eventing interface.
+  for dev := range net.EnableEvents() {
+    log.Printf("New device detected: %s\n", dev.Name)
+    log.Printf("Network:\n")
+    for _, d := range net.GetDevices() {
+      log.Printf(" * %s\n", d)
+    }
+  }
+}
