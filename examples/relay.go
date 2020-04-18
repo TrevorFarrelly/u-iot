c0dffb0a6db95eaf926f41102cef49e17156853c
Trevor Farrelly <farrellytrevor@gmail.com>
2020-04-18 17:19:14 -0400

    add more examples, update existing examples (#30)
    * examples: add Pi GPIO example programs
    
    Signed-off-by: Trevor Farrelly <farrellytrevor@gmail.com>
    
    * examples: improve error checking, add better comments
    
    Signed-off-by: Trevor Farrelly <farrellytrevor@gmail.com>

---
+++ examples/relay.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/TrevorFarrelly/rpi"
+	"github.com/TrevorFarrelly/u-iot/lib/uiot-go"
+)
+
+var ctrl = rpi.BoardToPin(5)
+
+// Set the control pin to HIGH.
+// Arguments are required to work with u-iot, due to Go's strict type system.
+// They are unused in this example
+func on(unused ...int) {
+	rpi.PinMode(ctrl, rpi.OUTPUT)
+	rpi.DigitalWrite(ctrl, rpi.HIGH, -1)
+}
+
+// set the control pin to LOW
+func off(unused ...int) {
+	rpi.PinMode(ctrl, rpi.OUTPUT)
+	rpi.DigitalWrite(ctrl, rpi.LOW, -1)
+}
+
+func main() {
+	// Parse command-line input
+	if len(os.Args) != 3 {
+		log.Printf("Invalid arguments.\nUsage: %s name port\n", os.Args[0])
+	}
+	name := os.Args[1]
+	port, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Printf("Could not parse port number: %v\n", err)
+	}
+
+	// set up wiring library
+	if err := rpi.WiringPiSetup(); err != nil {
+		log.Printf("Could not setup GPIO: %v\n", err)
+		return
+	}
+
+	// default pin to LOW
+	off()
+
+	// create new u-iot device with on/off functions
+	d := uiot.NewDevice(name, port, uiot.Light, uiot.Living)
+	d.AddFunction("on", on)
+	d.AddFunction("off", off)
+
+	// connect to the network
+	_, err := uiot.Bootstrap(d)
+	if err != nil {
+		log.Printf("could not bootstrap: %v", err)
+	}
+}
